58d682dd52524a86b03b6e70932bc475b636256f
Bryce Hammond <Bryce Hammond@BryceHammond-PC>
2014-10-21 15:49:20 -0700

    FIxed Oval and Rectangle

--- Netpaint/src/model/Oval.java
+++ Netpaint/src/model/Oval.java
@@ -6,9 +6,37 @@ import java.awt.Graphics;
 public class Oval extends Drawable {
 
 	public Oval(int x, int y, int height, int width, Color color) {
-		super(x, y, height, width, color);
+		super(xChange(x, width), yChange(y, height), heightChange(height), widthChange(width), color);
 	}
 
+	public static int xChange(int x, int width){
+		if(width < 0){
+			return x + width;
+		}
+		return x;
+	}
+	
+	public static int yChange(int y, int height){
+		if(height < 0){
+			return y + height;
+		}
+		return y;
+	}
+	
+	public static int heightChange(int height){
+		if(height < 0){
+			return Math.abs(height);
+		}
+		return height;
+	}
+	
+	public static int widthChange(int width){
+		if(width < 0){
+			return Math.abs(width);
+		}
+		return width;
+	}
+	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponents(g);
